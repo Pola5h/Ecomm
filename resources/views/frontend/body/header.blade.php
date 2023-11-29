47792c46524680b984430faecce492cee33d6a86
kzaman3055 <kzaman3055@gmail.com>
2023-11-29 14:52:35 +0600

    test

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -307,7 +307,7 @@
                 </div>
                 <div>
                     <p class="text-grayscales-900 inline-flex gap-2 items-center text-sm font-display">
-                        <span>Contact:</span><span class="text-secondary font-medium">(808) 555-0111</span>
+                        <span>Contact:</span><span class="text-secondary font-medium">01791296967</span>
                     </p>
                 </div>
             </div>
